d2759855fb47dbb45a1689b1379e482f224ba9f7
elypalacios <elizabethpalacios.cs@gmail.com>
2022-10-20 18:33:05 -0400

    Forms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from "react";
+import { useState } from 'react';
 import "./App.css";
 
 function City(props) {
+
+fetch('https://ctp-zip-api.herokuapp.com/')
+
   return <div>This is the City component</div>;
 }
 
 function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+
+const [zip, setZip] = useState("");
+const handleSubmit = (e) =>{
+  e.preventDefault();
+  const zipcode ={zip}
+  console.log(zipcode)
 }
+
+  return <div>
+  <form onSubmit= {handleSubmit}>
+   <label> Zip Code: 
+  <input 
+    type="text" 
+    value={zip} 
+    onChange={(e) => setZip(e.target.value)}>
+   </input>
+    </label> 
+  </form> 
+
+  <p> {zip} </p>
+   </div>;
+}
+
 
 function App() {
   return (
